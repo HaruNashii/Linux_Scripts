9269de18c80ac703374a649c4457b589db1af313
Haru <parajogapaladins@gmail.com>
2024-12-30 18:26:16 -0300

    improve cpu power script

--- change_cpu_power_mode.sh
+++ change_cpu_power_mode.sh
@@ -2,10 +2,35 @@
 
 # check if the command "cpupower" exist
 if command -v cpupower &> /dev/null; then
-	clear
-	# set the energy mode of your CPU to perfomance
-	sudo cpupower frequency-set --governor performance
-	echo "cpu power mode defined to 'perfomance'."
+
+	x=1
+	clear 
+	echo "What CPU Power Mode you want to Use?"
+	echo "1 = perfomance, 2 = powersave"
+	read -p "asnwer : " answer
+	while [ $x -le 2 ]; do
+		    case $answer in
+		        [1]*)
+				# set the energy mode of your CPU to powersave
+				sudo cpupower frequency-set --governor performance
+				clear
+				echo "cpu power mode defined to 'perfomance'."
+		            	break
+		            	;;
+		        [2]*)
+				# set the energy mode of your CPU to powersave
+				sudo cpupower frequency-set --governor powersave
+				clear
+				echo "cpu power mode defined to 'powersave'."
+		            	break
+		            	;;
+		        *)
+				echo "Invalid input. Please enter either '1' for (perfomance) or '2' for (powersave)."
+			    	read -p "asnwer : " answer
+		            	;;
+		    esac
+	done
+
 else 
 	clear && echo "cpupower command not found, please download it first!!"
 fi
