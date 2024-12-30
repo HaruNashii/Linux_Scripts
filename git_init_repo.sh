5daa67d24c89fec0e9f6eca3a4001a61cb32036a
Haru <parajogapaladins@gmail.com>
2024-12-30 15:05:31 -0300

    first commit

--- git_init_repo.sh
+++ git_init_repo.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-git init 
-git add .
-git commit -m "first commit"
-git branch -M main
-clear
-read -p "Your remote url: " remote_url 
-git remote add origin $remote_url 
-git push -u origin main
+if command -v git &> /dev/null; then
+	git init 
+	git add .
+	git commit -m "first commit"
+	git branch -M main
+	clear
+	read -p "Your remote url: " remote_url 
+	git remote add origin $remote_url 
+	git push -u origin main
+else
+	echo "git command not found, please download it before using this script!!!"
+fi
--- governator.sh
+++ governator.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# set the energy mode of your CPU to perfomance
-sudo cpupower frequency-set --governor performance
+# check if the command "cpupower" exist
+if command -v cpupower &> /dev/null; then
+	clear
+	# set the energy mode of your CPU to perfomance
+	sudo cpupower frequency-set --governor performance
+	echo "cpu power mode defined to 'perfomance'."
+else 
+	clear && echo "cpupower command not found, please download it first!!"
+fi
