9cb42b3b5573c40f1d1375b192a31afff63103e0
Haru <parajogapaladins@gmail.com>
2025-01-02 18:44:36 -0300

    add hyprland.conf

--- hyprland_config.sh
+++ hyprland_config.sh
@@ -11,36 +11,7 @@ fi
 
 # instala a configuracao do hyprland e do aplicativo de papel de parede
 rm -rf $HOME/.config/hypr
-
-x=1
-clear
-echo "Seu PC esta usando uma placa de video da NVIDIA???"
-echo "Responda com S ou N"
-read -p "resposta: " asnwer
-while [ $x -le 2 ]; do
-	case $asnwer in
-		[Ss]*)
-			#instala a configuracao do hyprland com uma config feita para placas da nvidia
-			mkdir -p $HOME/.config/hypr
-			rm -f $PWD/configs/hypr/hyprland.conf
-			mv $PWD/configs/hypr/hyprland_nvidia.conf $HOME/.config/hypr/hyprland.conf
-			cp -rf $PWD/configs/hypr/* $HOME/.config/hypr/
-			clear 
-			break
-			;;
-		[Nn]*)
-			#instala a configuracao do hyprland com uma config feita para placas que nao sao da nvidia
-			cp -rf $PWD/configs/hypr $HOME/.config/
-			clear 
-			break
-			;;
-		*)
-			echo "Respondeu Errado, e S, ou N"
-			read -p "resposta: " asnwer
-			;;
-	esac 
-done
-
+cp -rf $PWD/configs/hypr/ $HOME/.config/
 
 # move o wallpaper para a pasta que o aplicativo de papel de parede ler
 mkdir -p $HOME/Pictures/wallpapers
