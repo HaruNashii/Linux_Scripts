215605c1435d1602135c4cf5cefb1eb05d1f761e
Haru <parajogapaladins@gmail.com>
2025-05-06 16:08:42 -0300

    minor tweaks to osulazer download and configuration script

--- download_and_configure_osu_lazer.sh
+++ download_and_configure_osu_lazer.sh
@@ -1,24 +1,27 @@
-#======STRINGS=======
+#!/bin/bash
+# ^ define o script como script bash, para ser executado pelo propio bash
+
+
+#======VARIAVEIS=======
 bin_directory="$HOME/.local/bin"
 desktop_file_directory="$HOME/.local/share/applications"
 icon_file_directory="$HOME/Pictures/icons"
 file_to_download="$(curl -s https://api.github.com/repos/ppy/osu/releases/latest | grep browser_download_url | cut -d\" -f4 | grep .AppImage)"
-#====================
+#======================
+
 
 # cria a pasta do binario (se ela nao existir)
 mkdir -p $bin_directory
-
 # cria a pasta do atalho do desktop (se ela nao existir)
 mkdir -p $desktop_file_directory
-
 # cria a pasta do icone do osu (se ela nao existir)
 mkdir -p $icon_file_directory
 
-# entra na pasta do binario
-cd $bin_directory
 
 # verifica se o osu ja esta instalado, se estiver, nao baixa ele denovo :)
 if [ ! -f "$bin_directory/osu.AppImage" ]; then
+	# entra na pasta do binario
+	cd $bin_directory
 	# mostra um texto no terminal avisando que o Download do osu!lazer comecou
         echo "Baixando o osu!lazer, Por favor Espere..."
 	# baixa a ultima versao do osu!lazer, usando o comando "curl"
@@ -43,13 +46,17 @@ if [ ! -f "$desktop_file_directory/osu.desktop" ]; then
 	Categories=Game;"  >> "$desktop_file_directory/osu.desktop"
 fi 
 
+
 # verifica se o icon do osu ja esta instalado, se estiver nao baixa ele denovo :)
 if [ ! -f "$icon_file_directory/lazer.png" ]; then
 	# mostra um texto no terminal avisando que o Download do icone do osu!lazer comecou
         echo "Baixando o icone do osu!lazer, Por favor Espere..."
+	# entra na pasta do icone
 	cd $icon_file_directory
+	# baixa a ultima versao do icone do osu!lazer, usando o comando "curl"
 	curl -sLOJ https://raw.githubusercontent.com/ppy/osu/master/assets/lazer.png
 fi 
+
 
 # volta para a pasta principal do usuario
 cd $HOME
