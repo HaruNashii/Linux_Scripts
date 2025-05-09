9d5539a2025c9261cce420cc3c348c07e138053d
Haru <parajogapaladins@gmail.com>
2025-05-09 16:53:16 -0300

    update hyprland script

---
+++ hyprland_install_and_basic_configuration.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+# limpa o terminal
+clear
+
+
+# instala o hyprland, o terminal e o aplicativo de papel de parede do hyprland
+if command -v dnf &> /dev/null; then
+	# ativa o repositorio bleeding edge do hyprland
+	sudo dnf copr enable solopasha/hyprland
+
+	# instala o hyprland e seus utilitarios
+	sudo dnf install hyprland alacritty hyprpaper xdg-desktop-portal-hyprland grim slurp playerctl waybar
+
+	# instala a configuracao do hyprland e do aplicativo de papel de parede
+	rm -rf $HOME/.config/hypr
+	cp -rf $PWD/configs/hypr/ $HOME/.config/
+
+	# move o wallpaper para a pasta que o aplicativo de papel de parede ler
+	mkdir -p $HOME/Pictures/wallpapers
+	cp -f $PWD/configs/images/osu1.png $HOME/Pictures/wallpapers/
+	
+	# instala a configuracao do terminal do hyprland
+	rm -rf $HOME/.config/alacritty
+	cp -rf $PWD/configs/alacritty $HOME/.config/alacritty
+	
+	# instala a configuracao da barra do hyprland 
+	rm -rf $HOME/.config/waybar
+	cp -rf $PWD/configs/waybar $HOME/.config/waybar
+	
+	# instala a configuracao do app launcher do hyprland 
+	rm -rf $HOME/.config/wofi
+	cp -rf $PWD/configs/wofi $HOME/.config/wofi
+	
+	echo "pronto, agora e so logar no Hyprland :3"
+else 
+	echo "comando DNF nao encontrado????, esse comando e feito para o Fedora"
+fi 
+
