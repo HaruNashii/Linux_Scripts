993b3a94533b9774a95f32db6ab817750d04ebbf
Haru <parajogapaladins@gmail.com>
2025-09-08 17:33:38 -0300

    add script to kill genshin

---
+++ kill_genshin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pkill GenshinImpact
+pkill -f GenshinImpact.exe
+pkill genshinimpact
+pkill -f genshinimpact.exe
+pkill Hoyo
+pkill hoyo
+pkill HoyoPlay
+pkill Hoyoplay 
+pkill hoyoplay 
+pkill wine
+pkill wineservice
+pkill winedevice
+pkill windows
+pkill Windows
+pkill explorer
+pkill explorer.exe
